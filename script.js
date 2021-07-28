077126d00196ecd5bcbbaacafd304cca4a8a2c35
Katya <87824639+Katfv@users.noreply.github.com>
2021-07-28 18:02:30 +0000

    2

---
+++ script.js
@@ -0,0 +1,11 @@
+function howMany(){
+  var name = prompt("What is your name?");
+  var number = prompt ("Please enter a number between 1-10");
+  var celeb = prompt ("What is your favorite celebrity?");
+  var place = prompt ("What is yor favorite location in the world?");
+
+  var result = "Congratulations " + name + ", in " + number + " years you will meet " + celeb + " in " + place;
+
+  document.getElementById ("resultText").innerHTML = result;
+
+}
